0f918ceaffa1a5483f17eae9cbf0ce66bcdc34b8
KHW <rlagusdn6763@naver.com>
2020-06-05 21:56:22 +0900

    구글 로그인 컨트롤러 만들다 망함

--- src/main/java/com/kim/project/web/IndexController.java
+++ src/main/java/com/kim/project/web/IndexController.java
@@ -16,30 +16,4 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class IndexController {
 
-    private final PostsService postsService;
-
-    @GetMapping("/")
-    public String index(Model model, @LoginUser SessionUser user){
-        model.addAttribute("posts", postsService.findAllDesc());
-
-        if (user != null) {
-            model.addAttribute("userName", user.getName());
-        }
-
-        return "project/Login_v11/index"; //resources/templates/값.html
-    }
-
-    @GetMapping("/posts/save")
-    public String postsSave(){
-        return "posts-save";
-    }
-
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model){
-        PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post",dto);
-
-        return "posts-update";
-    }
-    }
 }
